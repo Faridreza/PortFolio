828b289062f0081c6815280bf313c4f1f2311987
Faridreza <FaridrezaBidkham@gmail.com>
2022-05-07 16:10:09 -0700

    PortFolio Web

---
+++ FaridrezaBidkham/FaridrezaBidkham/ViewComponents/SkillsViewComponent.cs
@@ -0,0 +1,10 @@
+﻿[ViewComponent(Name = "Skills")]
+public class SkillsViewComponent:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        string DataFile = File.ReadAllText("wwwroot\\Data\\Skills.txt");
+        IEnumerable<Experience>? Experience=JsonConvert.DeserializeObject<IEnumerable<Experience>>(DataFile);
+        return View("SkillsComponent", Experience);
+    }
+}
